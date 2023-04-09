dfbc952c8a527b2b27fd98df2293a5fbc33750a4
gavrilovava <113624355+gavrilovava@users.noreply.github.com>
2023-04-09 22:52:31 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -66,7 +66,7 @@ def table_record(value):
     del record.maxim[-1]
 
 def print_table():
-    value = font_style.render("Your Score: " + str(record.maxim[0]) + ', ' + str(record.maxim[1]) + ', ' + str(record.maxim[2]) + ', ' + str(record.maxim[3]) + ', ' + str(record.maxim[4]), True, colour.red)
+    value = font_style.render("Your Records: " + str(record.maxim[0]) + ', ' + str(record.maxim[1]) + ', ' + str(record.maxim[2]) + ', ' + str(record.maxim[3]) + ', ' + str(record.maxim[4]), True, colour.red)
     dis.blit(value, [parameter.dis_width / 6 + 80, parameter.dis_height / 3 + 40])
 
 dis = pygame.display.set_mode((parameter.dis_width, parameter.dis_height))
